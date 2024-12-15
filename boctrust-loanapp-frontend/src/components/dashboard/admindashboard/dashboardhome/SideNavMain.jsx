5dd905c87470e41fef988e50cc467ec8702dfdf1
Dr-AddictStein <dr.addictstein@gmail.com>
2024-12-16 02:57:21 +0600

    repayment-duplicate-fixed

--- boctrust-loanapp-frontend/src/components/dashboard/admindashboard/dashboardhome/SideNavMain.jsx
+++ boctrust-loanapp-frontend/src/components/dashboard/admindashboard/dashboardhome/SideNavMain.jsx
@@ -249,7 +249,7 @@ const SideNavMain = ({ onMenuItemClick }) => {
       </div>
 
       {/* repayment menu */}
-      <div
+      {/* <div
         className={`link__wrap  ${
           checkSectionActive("repayment") ? "section__active" : ""
         }`}
@@ -268,7 +268,7 @@ const SideNavMain = ({ onMenuItemClick }) => {
           <img id="repayment" src="/images/dtransfer.png" alt="repayment" />
           <p id="repayment">Repayments</p>
         </NavLink>
-      </div>
+      </div> */}
 
       {/* account menu */}
       <div
